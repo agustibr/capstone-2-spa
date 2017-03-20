ad1c5da2261d28a1c56b8b3bf85e6065df3f0276
Agustí B.R <hola@agusti.cat>
2017-03-20 23:37:29 +0100

    added redirect to client

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  get '/ui' => 'ui#index'
-  get '/ui#' => 'ui#index'
-  root 'ui#index'
+  scope :api, defaults: {format: :json}  do 
+    resources :foos, except: [:new, :edit]
+    resources :bars, except: [:new, :edit]
+  end      
+
+  get "/client-assets/:name.:format", :to => redirect("/client/client-assets/%{name}.%{format}")
+  get "/", :to => redirect("/client/index.html")
 end
